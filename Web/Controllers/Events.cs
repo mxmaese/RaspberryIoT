410c9331f9d45bae531c0ab6e13110afbc1249d3
mxmaese <53192708+mxmaese@users.noreply.github.com>
2025-06-12 23:29:39 -0300

    Configure JWT auth schemes

--- Web/Controllers/Events.cs
+++ Web/Controllers/Events.cs
@@ -1,12 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Services.Events;
 using Services.Web.Auth;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Api.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-[ApiTokenAuthentication]
+[Authorize(AuthenticationSchemes = "Events")]
 public class Events : ControllerBase
 {
     private readonly IEvents _eventsService;
